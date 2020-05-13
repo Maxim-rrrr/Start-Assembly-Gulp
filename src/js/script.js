151fd90ce1a506765adfd7b852c524ca6c92b7e5
Maxim <kewin.rrrr@gmail.com>
2020-05-13 23:26:06 +0300

    1.0

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+
+  // Подключение библиотек
+  @@include('import/wow.min.js')
+  
+  // Скрипты блоков
+  @@include('vendor/blocks.js')
+  
+  // Скрипт Gulp для подключение картинок в стилях
+  @@include('vendor/gulp.js')
+
+
+})
